369c5c420b13bfddc3b0060a012b5f0d6d944a70
ya95123 <ya95123@gmail.com>
2020-06-27 13:21:48 +0800

    修改 list 寬度

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,7 +2,6 @@
   <div id="list">
     <h3>待 辦 清 單</h3>
     <div id="addlist">
-      <!-- TODO 輸入完清空內容 -->
       <b-form-input id="input" v-model="newtodo" @keydown.enter="addTodo"></b-form-input>
        <b-btn id="input-btn" variant="outline-dark" @click="addTodo">新增</b-btn>
     </div>
@@ -10,7 +9,7 @@
       <b-thead>
         <b-tr>
           <b-th>事項</b-th>
-          <b-th>動作</b-th>
+          <b-th>編輯／刪除</b-th>
         </b-tr>
       </b-thead>
       <!-- 移動特效 -->
